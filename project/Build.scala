29723ee94e9ef14e9b7594ac458eb0e3cc04a84a
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-02 18:01:24 +0300

    Add Vaadin libs, remove Play from build

--- project/Build.scala
+++ project/Build.scala
@@ -6,18 +6,21 @@ object ApplicationBuild extends Build {
 
   val appName         = "reactive-stocks"
   val appVersion      = "1.0-SNAPSHOT"
-
+  val appScalaVersion = "2.10.1"
+    
   val appDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % "2.1.2",
     "com.typesafe.akka" %% "akka-slf4j" % "2.1.2",
-    "org.webjars" % "webjars-play" % "2.1.0-1",
-    "org.webjars" % "bootstrap" % "2.1.1",
-    "org.webjars" % "flot" % "0.8.0"
+    "com.vaadin" % "vaadin-server" % "7.1-SNAPSHOT",
+    "com.vaadin" % "vaadin-push" % "7.1-SNAPSHOT"
   )
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    scalaVersion := "2.10.1"
-  )
-
+  val appSettings = Seq(libraryDependencies := appDependencies,
+      					version := appVersion, 
+      					scalaVersion := appScalaVersion,
+      					resolvers := Seq("Vaadin Snapshots" at "https://oss.sonatype.org/content/repositories/vaadin-snapshots/"))
+  
+  lazy val app = Project(id = "reactive-stocks",
+                            base = file("."),
+                            settings = Project.defaultSettings ++ appSettings)
 }
