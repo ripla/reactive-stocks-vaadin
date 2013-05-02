0bbbbd0c9fa520df3d17c03ad3a8cd1ea2bce644
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-02 18:45:18 +0300

    Remove Play plugin, add Scaladin & Jetty

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,10 @@ object ApplicationBuild extends Build {
     "com.typesafe.akka" %% "akka-actor" % "2.1.2",
     "com.typesafe.akka" %% "akka-slf4j" % "2.1.2",
     "com.vaadin" % "vaadin-server" % "7.1-SNAPSHOT",
-    "com.vaadin" % "vaadin-push" % "7.1-SNAPSHOT"
+    "com.vaadin" % "vaadin-push" % "7.1-SNAPSHOT",
+    "vaadin.scala" %% "scaladin" % "3.0.0-SNAPSHOT",
+    "javax.servlet" % "servlet-api" % "2.4",
+    "org.eclipse.jetty" % "jetty-webapp" % "9.0.1.v20130408"
   )
 
   val appSettings = Seq(libraryDependencies := appDependencies,
