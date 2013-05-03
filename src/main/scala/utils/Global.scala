353160eadb29a66772bb2ea0ae263fb22a8a4b35
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-03 21:46:34 +0300

    Refactor Global to singleton

---
+++ src/main/scala/utils/Global.scala
@@ -0,0 +1,27 @@
+package utils
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import akka.actor.ActorSystem
+import akka.actor.Props
+import actors.UsersActor
+import actors.StockHolderActor
+import scala.concurrent.duration._
+import scala.concurrent.duration.Duration._
+import actors.FetchLatest
+
+object Global {
+  
+  val configuration = ConfigFactory.load();
+  val sentimentUrl = configuration.getString("sentiment.url")
+  val tweetUrl = configuration.getString("tweet.url")
+
+  val system = ActorSystem("reactive-stocks")
+  val usersActor = system.actorOf(Props[UsersActor], "users")
+  val stockHolderActor = system.actorOf(Props[StockHolderActor], "stocks")
+
+  import system.dispatcher
+
+  // fetch a new data point once every second
+  system.scheduler.schedule(Zero, 50 millis, stockHolderActor, FetchLatest.instance);
+}
