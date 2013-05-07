190360d6dc8349996c4dc4720fa3608774e65740
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-07 07:40:17 +0300

    Create StockPresenter as a typed actor

--- src/main/scala/actors/UsersActor.scala
+++ src/main/scala/actors/UsersActor.scala
@@ -3,15 +3,22 @@ package actors;
 import akka.actor.ActorRef
 import akka.actor.Props
 import akka.actor.UntypedActor
-import akka.japi.Creator;
+import akka.japi.Creator
 import akka.actor.Actor
+import akka.actor.TypedActor
+import akka.actor.TypedProps
+import ui.StockPresenterImpl
+import ui.StockPresenter
+import ui.Listen
+import ui.WatchStock
+import ui.UnwatchStock
 
 class UsersActor extends Actor {
 
   def receive = {
     case message: StockUpdate => context.children.foreach(child => child ! message)
 
-    case message: Listen => sender ! context.actorOf(Props[UserActor], message.uuid)
+    case message: Listen => sender ! TypedActor(context).typedActorOf(TypedProps(classOf[StockPresenter], new StockPresenterImpl(message.uuid)), message.uuid) 
 
     case message: WatchStock => for (child <- context.child(message.uuid)) yield child ! message
 
