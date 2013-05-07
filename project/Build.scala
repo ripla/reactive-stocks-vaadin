d9d3adfa5811ba9fe29e8c0216334217347a6046
Risto Yrjänä <risto.yrjana@vaadin.com>
2013-05-07 07:41:03 +0300

    Add missing dependencies, setup web.xml

--- project/Build.scala
+++ project/Build.scala
@@ -1,21 +1,28 @@
 import sbt._
 import Keys._
-import play.Project._
+import com.github.siasia.WebPlugin._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "reactive-stocks"
-  val appVersion      = "1.0-SNAPSHOT"
-  val appScalaVersion = "2.10.1"
-    
+  val appName           = "reactive-stocks"
+  val appVersion        = "1.0-SNAPSHOT"
+  val appScalaVersion   = "2.10.1"
+  val appVaadinVersion  = "7.1.0.beta1"
+
   val appDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % "2.1.2",
     "com.typesafe.akka" %% "akka-slf4j" % "2.1.2",
-    "com.vaadin" % "vaadin-server" % "7.1-SNAPSHOT",
-    "com.vaadin" % "vaadin-push" % "7.1-SNAPSHOT",
+    "com.vaadin" % "vaadin-server" % appVaadinVersion,
+    "com.vaadin" % "vaadin-push" % appVaadinVersion,
+    "com.vaadin" % "vaadin-client-compiled" % appVaadinVersion,
+    "com.vaadin" % "vaadin-themes" % appVaadinVersion,
     "vaadin.scala" %% "scaladin" % "3.0.0-SNAPSHOT",
-    "javax.servlet" % "servlet-api" % "2.4",
-    "org.eclipse.jetty" % "jetty-webapp" % "9.0.1.v20130408",
+    "javax.servlet" % "javax.servlet-api" % "3.0.1",
+    "org.eclipse.jetty" % "jetty-webapp" % "8.1.10.v20130312" % "container",
+    "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container",
+    "org.eclipse.jetty" % "jetty-continuation" % "8.1.10.v20130312",
+    "org.eclipse.jetty" % "jetty-websocket" % "8.1.10.v20130312",
+    //"org.eclipse.jetty", "jetty-annotations", % "8.1.10.v20130312" % "container"
     "com.vaadin.addon" % "vaadin-charts" % "1.0.1",
     "com.typesafe" % "config" % "1.0.0"
   )
@@ -26,7 +33,7 @@ object ApplicationBuild extends Build {
   val appSettings = Seq(libraryDependencies := appDependencies,
       					version := appVersion, 
       					scalaVersion := appScalaVersion,
-      					resolvers := repositories)
+      					resolvers := repositories) ++ webSettings
   
   lazy val app = Project(id = "reactive-stocks",
                             base = file("."),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,3 +3,7 @@ logLevel := Level.Warn
 
 // The Typesafe repository 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "Web plugin repo" at "http://siasia.github.com/maven2"  
+
+libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin" % "0.12.0-0.2.11.1")
