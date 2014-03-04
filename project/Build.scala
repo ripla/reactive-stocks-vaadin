3e5b3f53214827422dfb6d10e19b8b686e96522a
Risto Yrjänä <risto.yrjana@vaadin.com>
2014-03-04 16:57:31 +0200

    Update dependency versions

--- project/Build.scala
+++ project/Build.scala
@@ -1,13 +1,12 @@
 import sbt._
 import Keys._
 import org.vaadin.sbt.VaadinPlugin._
-//import com.earldouglas.xsbtwebplugin.WebPlugin._
 object ApplicationBuild extends Build {
 
   val appName           = "reactive-stocks"
   val appVersion        = "1.0-SNAPSHOT"
-  val appScalaVersion   = "2.10.2"
-  val appVaadinVersion  = "7.1.7"
+  val appScalaVersion   = "2.10.3"
+  val appVaadinVersion  = "7.1.11"
 
   val appDependencies = Seq(
     "com.typesafe.akka" %% "akka-actor" % "2.2.1",
@@ -16,26 +15,25 @@ object ApplicationBuild extends Build {
     "com.vaadin" % "vaadin-push" % appVaadinVersion,
     "com.vaadin" % "vaadin-themes" % appVaadinVersion,
     "com.vaadin" % "vaadin-client-compiler" % appVaadinVersion % "provided",
-    "vaadin.scala" %% "scaladin" % "3.0-SNAPSHOT" changing(),
+    "org.vaadin.addons" % "scaladin" % "3.0.0",
     "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided",
     "org.eclipse.jetty" % "jetty-webapp" % "8.1.10.v20130312" % "container",
     "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container",
     "org.eclipse.jetty" % "jetty-continuation" % "8.1.10.v20130312" % "container",
     "org.eclipse.jetty" % "jetty-websocket" % "8.1.10.v20130312" % "container",
     //"org.eclipse.jetty", "jetty-annotations", % "8.1.10.v20130312" % "container"
-    "com.vaadin.addon" % "vaadin-charts" % "1.1.2",
+    "com.vaadin.addon" % "vaadin-charts" % "1.1.5",
     "play" %% "play" % "2.1.5"
   )
-  
+
   val repositories = Seq("Vaadin Snapshots" at "https://oss.sonatype.org/content/repositories/comvaadin-1030",
                          "Vaadin add-ons" at "http://maven.vaadin.com/vaadin-addons",
                          "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/")
 
-  val appSettings = 
-  vaadinWebSettings ++ 
-  //webSettings ++
+  val appSettings =
+  vaadinWebSettings ++
   Seq(libraryDependencies := appDependencies,
-      					version := appVersion, 
+      					version := appVersion,
       					scalaVersion := appScalaVersion,
       					resolvers := repositories,
                 javaOptions in compileVaadinWidgetsets := Seq("-Xss8M", "-Xmx512M", "-XX:MaxPermSize=512M"),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,13 +1,11 @@
 // Comment to get more information during initialization
 logLevel := Level.Warn
 
-// The Typesafe repository 
+// The Typesafe repository
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "0.4.2")
+resolvers += "sbt-vaadin-plugin repo" at "http://henrikerola.github.io/repository/releases"
 
-resolvers += "sbt-vaadin-plugin repo" at "http://henrikerola.github.io/repository/snapshots"
-
-addSbtPlugin("org.vaadin.sbt" % "sbt-vaadin-plugin" % "0.1-SNAPSHOT")
+addSbtPlugin("org.vaadin.sbt" % "sbt-vaadin-plugin" % "1.0.0")
 
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.3.0")
